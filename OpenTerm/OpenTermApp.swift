6d7fd41e8edf532d6f7ff7a705b61695af775893
bentech4u <bentech4you@gmail.com>
2026-02-04 07:25:35 +0300

    Initial Commit

---
+++ OpenTerm/OpenTermApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenTermApp.swift
+//  OpenTerm
+//
+//  Created by Ben George on 04/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct OpenTermApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
